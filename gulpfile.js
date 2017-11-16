a6e538642154369064c98a47571801567ed5f44a
anatol06 <anatol06@gmail.com>
2017-11-16 20:07:49 +0100

    Added Create and Share sections, footer, modal, soft scrolling

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ gulp.task('sass', function() {
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 
                      'node_modules/jquery/dist/jquery.min.js',
+                     'node_modules/jquery.easing/jquery.easing.min.js',
                      'node_modules/popper.js/dist/umd/popper.min.js'])
         .pipe(gulp.dest('src/js'))
         .pipe(browserSync.stream());
