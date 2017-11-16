36e8450c3e172e134bf082daec14eef48c75d9a1
anatol06 <anatol06@gmail.com>
2017-11-16 20:53:12 +0100

    Edited gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ const gulp = require('gulp'),
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','docs/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest('docs/css'))
         .pipe(browserSync.stream());
 });
 
-// Move JS Files to src/js
+// Move JS Files to docs/js
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 
                      'node_modules/jquery/dist/jquery.min.js',
@@ -27,17 +27,17 @@ gulp.task('serve',['sass'], function() {
         notify: false,
         server: "./docs"
     });
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'docs/scss/*.scss'], ['sass']);
     gulp.watch('docs/*.html').on('change', browserSync.reload);
 });
 
-// Mov Fonts Folder to src/fonts
+// Mov Fonts Folder to docs/fonts
 gulp.task('fonts', function() {
     return gulp.src('node_modules/font-awesome/fonts/*')
         .pipe(gulp.dest('docs/fonts'));
 });
 
-// Mov Font Awesome CSS to src/css
+// Mov Font Awesome CSS to docs/css
 gulp.task('fa', function () {
     return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
         .pipe(gulp.dest('docs/css'));
